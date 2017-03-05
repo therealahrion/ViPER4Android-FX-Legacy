bb14eb6bea29d6706cf6da908580ea6fb1a61598
ahrion <Ahrionmgallegos@gmail.com>
2017-03-05 13:53:56 -0700

    typo fix

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,7 +6,7 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-MODID=v4a
+MODID=v4afx
 AUDMODLIBPATH=/magisk/audmodlib
 source $MODDIR/module.prop
 
