7faaa61f6c00a6119a11aa6db0446b3e4363b79b
ahrion <Ahrionmgallegos@gmail.com>
2017-08-20 15:48:56 -0600

    add conditional statements instead of sed removing lines

--- common/unity-patches.sh
+++ common/unity-patches.sh
@@ -6,15 +6,9 @@
 #
 #
 # ^ DO NOT MODIFY ^
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-    sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$CFG
-    sed -i '/v4a_fx {/,/}/d' $AMLPATH$CFG
-  fi
-done
 ui_print "    Patching existing audio_effects files..."
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
+  if [ -f $CFG ] && [ ! "$(cat $AMLPATH$CFG | grep ' v4a_fx {')" ]; then
     sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $AMLPATH$CFG
     sed -i 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' $AMLPATH$CFG
   fi
