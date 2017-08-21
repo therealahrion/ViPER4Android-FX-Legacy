e11ac365c117ae968fab39ab4b2a116802e28e8d
Zackptg5 <Zackptg5@comcast.net>
2017-08-21 19:11:53 -0400

    Add dynamic addon.d logic

--- system/addon.d/modid.sh
+++ system/addon.d/modid.sh
@@ -1,12 +1,10 @@
 #!/sbin/sh
-# 
-# /system/addon.d/v4afx.sh
 #
 
 . /tmp/backuptool.functions
 
 #### v INSERT YOUR CONFIG.SH MODID v ####
-MODID=v4afx
+MODID=<MODID>
 AUDMODLIBID=audmodlib
 #### ^ INSERT YOUR CONFIG.SH MODID ^ ####
 
@@ -85,31 +83,6 @@ case "$1" in
 	# Stub
   ;;
   post-restore)
-    #### v INSERT YOUR FILE PATCHES v ####
-    # REMOVE LIBRARIES & EFFECTS
-    for CFG in $CONFIG_FILE $OFFLOAD_CONFIG $OTHER_V_FILE $HTC_CONFIG_FILE $V_CONFIG_FILE; do
-      if [ -f $CFG ]; then
-        # REMOVE EFFECTS
-        sed -i '/v4a_standard_fx {/,/}/d' $CFG
-        # REMOVE LIBRARIES
-        sed -i '/v4a_fx {/,/}/d' $CFG
-      fi
-    done
-
-    # ADD LIBRARIES & EFFECTS
-    for CFG in $CONFIG_FILE $OFFLOAD_CONFIG $OTHER_V_FILE $HTC_CONFIG_FILE $V_CONFIG_FILE; do
-      if [ -f $CFG ]; then
-        # ADD EFFECTS
-        sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $CFG
-        # ADD LIBRARIES
-        sed -i 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' $CFG
-      fi
-    done
-
-    # COPY OVER MAIN AUDIO_EFFECTS CFG FILE TO VEN FILE
-    if [ -f $V_CONFIG_FILE ]; then
-      cp -af $CONFIG_FILE $V_CONFIG_FILE
-    fi
-    #### ^ INSERT YOUR FILE PATCHES ^ ####
+    <PATCHES>
   ;;
 esac
