7a998740be7e8dca62b4d5cb1054857876d0a659
Unknown <zackptg5@comcast.net>
2017-08-27 14:35:11 -0400

    Update instructions, make config.sh universal, bug fixes

--- common/unity-patches.sh
+++ common/unity-patches.sh
@@ -1,10 +1,8 @@
 # v DO NOT MODIFY v
 # See instructions file for predefined variables
-# Add patches (such as audio config) here (starting at line 9)
+# Add patches (such as audio config) here
 # NOTE: Destination variable must have '$AMLPATH' in front of it
 # Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$CONFIG_FILE
-#
-#
 # ^ DO NOT MODIFY ^
 ui_print "    Patching existing audio_effects files..."
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
