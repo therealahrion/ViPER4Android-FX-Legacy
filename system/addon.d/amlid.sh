c768b9322aceba8ad819ac555b910ca456a7ccaa
Zackptg5 <zackptg5@comcast.net>
2017-10-02 09:48:24 -0400

    Add capability for /data modifications, improve addon.d
    Modify addon.d scripts so "system" no longer needs removed. Makes life much easier. Add minVer variable to module.prop for instances like these where the template is for the beta version but mod should be backwardsly compatible with stable version

--- system/addon.d/amlid.sh
+++ system/addon.d/amlid.sh
@@ -53,14 +53,14 @@ EOF
 case "$1" in
   backup)
     list_files | while read FILE DUMMY; do
-      backup_file $S/$FILE
+      backup_file $FILE
     done
   ;;
   restore)
     list_files | while read FILE REPLACEMENT; do
       R=""
-      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
-      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+      [ -n "$REPLACEMENT" ] && R="$REPLACEMENT"
+      [ -f "$C/$FILE" ] && restore_file $FILE $R
     done
   ;;
   pre-backup)
