6e735b99a3ea822a5e206e5200c2007b549b5b32
therealahrion <ahrionmgallegos@gmail.com>
2017-10-21 10:59:18 -0600

    cleanup unnecessary wipe

--- common/unity-files-wipe.sh
+++ common/unity-files-wipe.sh
@@ -1,7 +1,2 @@
 FOL/data/app/com.vipercn.viper4android_v2-1
-FOL/data/app/com.vipercn.viper4android_v2-2
-FOL/data/app/com.vipercn.viper4android_v2-3
 /data/app/com.audlabs.viperfx-1
-/data/app/com.audlabs.viperfx-2
-APPViPER4AndroidFX
-$SYS/lib/soundfx/libv4a_fx_jb_$DRVARCH.so
