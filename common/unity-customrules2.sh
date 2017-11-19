8e499efaf73f5dada701ba5f3b33acc2ddd80f14
Zackptg5 <zackptg5@comcast.net>
2017-11-19 15:38:34 -0500

    Add zipname capability
    user can add old or new to the zipname to specify which version they want installed

--- common/unity-customrules2.sh
+++ common/unity-customrules2.sh
@@ -1,5 +1,11 @@
 TIMEOFEXEC=1
 
+# GET OLD/NEW FROM ZIP NAME
+case $(basename $ZIP) in
+  *old*|*Old*|*OLD*) UI=21;;
+  *new*|*New*|*NEW*) UI=42;;
+esac
+
 # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
 KEYCHECK=$INSTALLER/common/keycheck
 chmod 755 $KEYCHECK
@@ -10,7 +16,7 @@ chooseport() {
   if [[ $INPUT -eq 42 ]] || [[ $INPUT -eq 21 ]]; then
     UI=$INPUT
   else
-    ui_print "Unrecognized key! Using new V4A"
+    ui_print "   Unrecognized key! Using new V4A!"
     UI=42
   fi
   shift
@@ -18,10 +24,14 @@ chooseport() {
 
 ui_print " "
 ui_print "- Select Version -"
-ui_print "   Choose which V4A you want installed:"
-ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
-ui_print "   Old V4A will install super quality driver"
-chooseport
+if [ -z $UI ]; then
+  ui_print "   Choose which V4A you want installed:"
+  ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
+  ui_print "   Old V4A will install super quality driver"
+  chooseport
+else
+  ui_print "   V4A version specified in zipname!"
+fi
 if [[ $UI -eq 21 ]]; then
   ui_print "   Old V4A will be installed"
   cp -f $INSTALLER/custom/Old/ViPER4AndroidFX.apk $INSTALLER/system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
