57b68bebe2d9633e4ccad0ea569624b6681e8696
Zackptg5 <zackptg5@comcast.net>
2017-11-26 12:48:40 -0500

    Get rid of material v4a

--- common/unity-customrules2.sh
+++ common/unity-customrules2.sh
@@ -2,9 +2,8 @@ TIMEOFEXEC=1
 
 # GET OLD/NEW FROM ZIP NAME
 case $(basename $ZIP) in
-  *old*|*Old*|*OLD*) UI=21; MAT=false;;
-  *orig*|*Orig*|*ORIG*) UI=42; MAT=false;;
-  *mat*|*Mat*|*MAT*) UI=42; MAT=true;;
+  *old*|*Old*|*OLD*) UI=21;;
+  *new*|*New*|*NEW*) UI=42;;
 esac
 
 # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
@@ -25,7 +24,7 @@ chooseport() {
 
 ui_print " "
 ui_print "- Select Version -"
-if [ -z $MAT ]; then
+if [ -z $UI ]; then
   ui_print "   Choose which V4A you want installed:"
   ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
   ui_print "   Old V4A will install super quality driver"
@@ -40,23 +39,6 @@ if [[ $UI -eq 21 ]]; then
   cp -f $INSTALLER/custom/Old/libv4a_fx_jb_X86.so $INSTALLER/custom/libv4a_fx_jb_X86.so
   sed -ri "s/version=(.*)/version=\1 (2.3.4.0)/" $INSTALLER/module.prop
 else
-  UI=""
-  if [ -z $MAT ]; then
-    ui_print "   New V4A will be installed"
-    ui_print "   Choose which V4A you want installed:"
-    ui_print "   Vol+ = original, Vol- = materialized"
-    sleep 1
-    chooseport
-  elif [ "$MAT" == true ]; then
-	UI=21
-  fi
-  if [[ $UI -eq 21 ]]; then
-    ui_print "   Materialized V4A by pittvandewit will be installed"
-    sed -ri "s/version=(.*)/version=\1 (2.5.0.5 Materialized)/" $INSTALLER/module.prop
-    sed -ri "s/author=(.*)/author=\1, pittvandewit/" $INSTALLER/module.prop
-    cp -f $INSTALLER/custom/Materialized/ViPER4AndroidFX.apk $INSTALLER/system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
-  else
-    ui_print "   Original V4A will be installed"
-    sed -ri "s/version=(.*)/version=\1 (2.5.0.5)/" $INSTALLER/module.prop
-  fi
+  ui_print "   New V4A will be installed"
+  sed -ri "s/version=(.*)/version=\1 (2.5.0.5)/" $INSTALLER/module.prop
 fi
