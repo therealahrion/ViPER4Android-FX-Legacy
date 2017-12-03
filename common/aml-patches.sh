b70de0884e70a15cdf0aba3c300297ed5f9dce6d
Zackptg5 <zackptg5@comcast.net>
2017-12-03 11:09:26 -0500

    fix stuff

--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,5 +1,5 @@
 ui_print "    Patching existing audio_effects files..."
-if [ $API -ge 26 ]; then TMP="$(echo $VEN | sed "s|/|\/|g")"; else TMP="\/system"; fi
+if [ $API -ge 26 ]; then TMP="$(echo $VEN | sed "s|/|\\\/|g")"; else TMP="\/system"; fi
 for FILE in ${CFGS}; do
   sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $AMLPATH$FILE
   sed -i "s/^libraries {/libraries {\n  v4a_fx {\n    path $TMP\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g" $AMLPATH$FILE
