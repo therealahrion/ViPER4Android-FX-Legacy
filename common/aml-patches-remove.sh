8336d082432684e4a9aa0a840607aa9a2074556f
Zackptg5 <zackptg5@comcast.net>
2017-12-02 19:17:54 -0500

    Massive consolidation. Remove xml-patch files, make part of regular patch files

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -2,3 +2,6 @@ for FILE in ${CFGS}; do
   sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$FILE
   sed -i '/v4a_fx {/,/}/d' $AMLPATH$FILE
 done
+# if [ ! -z $XML_PRFX ]; then
+  # # Enter xmlstarlet logic here
+# fi
