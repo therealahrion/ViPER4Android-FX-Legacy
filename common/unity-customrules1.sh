21787bd5e414241e066291f59514a7dccf0c8264
Zackptg5 <zackptg5@comcast.net>
2017-12-03 21:34:10 -0500

    Add logic to aml
    Have all files in app and lib folders be installed vendor if oreo or newer

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -1,5 +1,5 @@
 TIMEOFEXEC=3
-if [ $API -ge 26 ]; then
+if $OREONEW; then
   $CP_PRFX $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $UNITY$VEN/lib/soundfx/libv4a_fx_ics.so
 else
   $CP_PRFX $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $UNITY$SYS/lib/soundfx/libv4a_fx_ics.so
