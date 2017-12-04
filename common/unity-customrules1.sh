b049af325e28a27d496e4d045abc9f4da366bbd8
Zackptg5 <zackptg5@comcast.net>
2017-12-03 21:56:59 -0500

    Make fully part of aml

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -1,6 +1,2 @@
 TIMEOFEXEC=3
-if $OREONEW; then
-  $CP_PRFX $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $UNITY$VEN/lib/soundfx/libv4a_fx_ics.so
-else
-  $CP_PRFX $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $UNITY$SYS/lib/soundfx/libv4a_fx_ics.so
-fi
+$CP_PRFX $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $UNITY$LIBDIR/lib/soundfx/libv4a_fx_ics.so
