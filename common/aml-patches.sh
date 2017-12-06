4c4852f374d49ada2c9413f8b49fcc5623350b99
Zackptg5 <zackptg5@comcast.net>
2017-12-06 13:12:50 -0500

    Remove oreonew stuff - no need for it

--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,7 +1,7 @@
 ui_print "    Patching existing audio_effects files..."
 for FILE in ${CFGS}; do
   sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $AMLPATH$FILE
-  sed -i "s/^libraries {/libraries {\n  v4a_fx {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g" $AMLPATH$FILE
+  sed -i 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' $AMLPATH$FILE
 done
 # if [ ! -z $XML_PRFX ]; then
   # # Enter xmlstarlet logic here
