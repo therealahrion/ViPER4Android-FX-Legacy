23cc0f9eb8983241aa05b8b48fe6cdd5b6504b6b
Zackptg5 <zackptg5@comcast.net>
2017-12-06 22:30:42 -0500

    Test adding audio effect xml file support

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,6 +1,11 @@
 for FILE in ${CFGS}; do
-  sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$FILE
-  sed -i '/v4a_fx {/,/}/d' $AMLPATH$FILE
+  if [ "$FILE" == "*.conf" ]; then
+    sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$FILE
+    sed -i '/v4a_fx {/,/}/d' $AMLPATH$FILE
+  elif [ "$FILE" == "*.xml" ]; then
+    sed -i '/v4a_standard_fx/d' $AMLPATH$FILE
+    sed -i '/v4a_fx/d' $AMLPATH$FILE
+  fi
 done
 # if [ ! -z $XML_PRFX ]; then
   # # Enter xmlstarlet logic here
