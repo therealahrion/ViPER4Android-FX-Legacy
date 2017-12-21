6c8e658b2a9c3a1180fc4a0114017ee9ea1d3624
Zackptg5 <zackptg5@comcast.net>
2017-12-20 19:08:38 -0500

    Script bug fix, separate sepolicy patches to each module

---
+++ common/post-fs-data.sh
@@ -0,0 +1,8 @@
+set_sepolicy audioserver audioserver_tmpfs file read,write,execute
+set_sepolicy audioserver system_file file execmod
+set_sepolicy mediaserver mediaserver_tmpfs file read,write,execute
+set_sepolicy mediaserver system_file file execmod
+set_sepolicy audioserver unlabeled file read,write,open,getattr,execute
+set_sepolicy hal_audio_default hal_audio_default process execmem
+set_sepolicy hal_audio_default hal_audio_default_tmpfs file execute
+set_sepolicy hal_audio_default audio_data_file dir search
