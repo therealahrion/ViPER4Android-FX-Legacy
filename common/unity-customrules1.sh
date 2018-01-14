55916b7023e6f4fcb75ab685d3bb743ed1d07eff
Zackptg5 <zackptg5@comcast.net>
2018-01-14 11:02:44 -0500

    Found new way of doing this in chainfire's bootsign zip

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -16,57 +16,55 @@ if $OREONEW && [ "$(grep_prop ro.product.brand)" == "OnePlus" ]; then
   echo "setenforce 0" >> $INSTALLER/common/unity-audmodlib/$MODID-post-fs-data.sh
 fi
 
+OLD=false; NEW=false; MAT=false
 # GET OLD/NEW FROM ZIP NAME
 case $(basename $ZIP) in
-  *old*|*Old*|*OLD*) UI=21; MAT=21;;
-  *new*|*New*|*NEW*) UI=42; MAT=21;;
-  *mat*|*Mat*|*MAT*) UI=42; MAT=42;;
+  *old*|*Old*|*OLD*) OLD=true;;
+  *new*|*New*|*NEW*) NEW=true;;
+  *mat*|*Mat*|*MAT*) MAT=true;;
 esac
 
-# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
-KEYCHECK=$INSTALLER/common/keycheck
-chmod 755 $KEYCHECK
-
 chooseport() {
-  $KEYCHECK
-  INPUT=$?
-  if [[ $INPUT -eq 42 ]] || [[ $INPUT -eq 21 ]]; then
-    UI=$INPUT
-  else
-    ui_print "   ! Unrecognized key!"
-    UI=42
+  if [ "$1" == "on" ]; then
+    ui_print "   Choose which V4A you want installed:"
+    ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
+    ui_print "   Old V4A will install super quality driver"
+  elif [ "$1" == "mn" ]; then
+    ui_print "   Choose which new V4A you want installed:"
+    ui_print "   Vol+ = material, Vol- = original"
   fi
-  shift
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while (true); do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
 }
 
 mkdir -p $INSTALLER/system/lib/soundfx
 ui_print " "
 ui_print "- Select Version -"
-if [ -z $MAT ]; then
-  ui_print "   Choose which V4A you want installed:"
-  ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
-  ui_print "   Old V4A will install super quality driver"
-  chooseport
+if ! $OLD && ! $NEW && ! $MAT; then
+  chooseport on || OLD=true
 else
   ui_print "   V4A version specified in zipname!"
 fi
-if [[ $UI -eq 21 ]]; then
+if $OLD; then
   ui_print "   Old V4A will be installed"
   cp -f $INSTALLER/custom/Old/ViPER4AndroidFX.apk $INSTALLER/system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
   cp -f $INSTALLER/custom/Old/libv4a_fx_jb_$DRVARCH.so $INSTALLER/system/lib/soundfx/libv4a_fx_ics.so
   sed -ri "s/version=(.*)/version=\1 (2.3.4.0)/" $INSTALLER/module.prop
   $OREONEW && { sed -i "s|/vendor|/system|g" $INSTALLER/common/aml-patches.sh; LIBDIR=$SYS; OREONEW=false; }
 else
+  $NEW || $MAT || { chooseport mn && MAT=true; }
   cp -f $INSTALLER/custom/libv4a_fx_jb_$DRVARCH.so $INSTALLER/system/lib/soundfx/libv4a_fx_ics.so
-  if [ -z $MAT ]; then
-    sleep 1
-    ui_print "   Choose which new V4A you want installed:"
-    ui_print "   Vol+ = material, Vol- = original"
-    chooseport
-  else
-    UI=$MAT
-  fi
-  if [[ $UI -eq 42 ]]; then
+  if $MAT; then
     ui_print "   Material V4A will be installed"
     cp -f $INSTALLER/custom/ViPER4AndroidFXMaterial.apk $INSTALLER/system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
     sed -ri -e "s/version=(.*)/version=\1 (2.5.0.5)/" -e "s/author=(.*)/author=\1, pittvandewitt/" -e "s/name=(.*)/name=\1 Materialized/" $INSTALLER/module.prop
