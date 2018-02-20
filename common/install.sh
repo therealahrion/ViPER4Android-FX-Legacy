b560d6d6b86dd67581da71f5321168a61e1140ab
Zackptg5 <zackptg5@comcast.net>
2018-02-20 14:38:04 -0500

    Fix for essential phone oreo

--- common/install.sh
+++ common/install.sh
@@ -1,7 +1,7 @@
 rm -rf /data/app/com.pittvandewitt.viperfx /data/app/com.vipercn.viper4android* /data/app/com.audlabs.viperfx*
 
 # Device specific sepolicy patches
-if device_check "walleye" || device_check "taimen"; then
+if device_check "walleye" || device_check "taimen" || device_check "mata"; then
   test -f $SYS/lib/libstdc++.so && cp_ch $SYS/lib/libstdc++.so $UNITY$VEN/lib/libstdc++.so
 fi
 
