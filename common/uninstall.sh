972a08036387449e18a28720c0afaf8b848457f6
Zackptg5 <zackptg5@comcast.net>
2018-04-12 22:05:09 -0400

    no need for this anymore

--- common/uninstall.sh
+++ common/uninstall.sh
@@ -1,5 +1,3 @@
-MATV4A=$(find /data/app -type d -name "*com.pittvandewitt.viperfx*")
-[ "$MATV4A" ] && if $BOOTMODE; then ui_print "   Uninstalling V4A app..."; pm uninstall com.pittvandewitt.viperfx; else ui_print "   Removing V4A app..."; rm -rf $MATV4A; fi
 $MAGISK || { for OFILE in ${CFGS}; do
                FILE="$UNITY$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
                case $FILE in
