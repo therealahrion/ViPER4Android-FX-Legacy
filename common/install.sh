71795c89bf55fba9e2ea0236e1040894deed69e1
Zackptg5 <zackptg5@comcast.net>
2018-12-06 14:09:29 -0500

    Add aud pol conf usb fix

--- common/install.sh
+++ common/install.sh
@@ -95,7 +95,7 @@ echo $PATH | grep -q "^$INSTALLER/common/xmlstarlet/$ARCH32" || export PATH=$INS
 # Tell user aml is needed if applicable
 if $MAGISK && ! $SYSOVERRIDE; then
   if $BOOTMODE; then LOC="/sbin/.core/img/*/system $MOUNTPATH/*/system"; else LOC="$MOUNTPATH/*/system"; fi
-  FILES=$(find $LOC -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml" -o -name "usb_audio_policy_configuration.xml" 2>/dev/null)
+  FILES=$(find $LOC -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml" -o -name "usb_audio_policy_configuration.xml" -o -name "*audio_*policy*.conf" 2>/dev/null)
   if [ ! -z "$FILES" ] && [ ! "$(echo $FILES | grep '/aml/')" ]; then
     ui_print " "
     ui_print "   ! Conflicting audio mod found!"
@@ -308,17 +308,28 @@ if device_check "walleye" || device_check "taimen" || device_check "crosshatch" 
 fi
 
 ui_print " "
-ui_print "   Applying fixes for usb output..."
-for OFILE in ${UPCS}; do
-  FILE="$UNITY$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
-  cp_ch -nn $ORIGDIR$OFILE $FILE
-  grep -iE " name=\"usb[ _]+.* output\"" $FILE | sed -r "s/.*ame=\"([A-Za-z_ ]*)\".*/\1/" | while read i; do
-    patch_xml $FILE "/module/mixPorts/mixPort[@name=\"$i\"]/profile[@name=\"\"]"
+if [ "$UPCS" ]; then
+  ui_print "   Applying fixes for usb output..."
+  for OFILE in ${UPCS}; do
+    FILE="$UNITY$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
+    cp_ch -nn $ORIGDIR$OFILE $FILE
+    grep -iE " name=\"usb[ _]+.* output\"" $FILE | sed -r "s/.*ame=\"([A-Za-z_ ]*)\".*/\1/" | while read i; do
+      patch_xml $FILE "/module/mixPorts/mixPort[@name=\"$i\"]/profile[@name=\"\"]"
+    done
+    grep -iE "tagName=\"usb[ _]+.* out\"" $FILE | sed -r "s/.*ame=\"([A-Za-z_ ]*)\".*/\1/" | while read i; do
+      patch_xml $FILE "/module/devicePorts/devicePort[@tagName=\"$i\"]/profile[@name=\"\"]"
+    done
   done
-  grep -iE "tagName=\"usb[ _]+.* out\"" $FILE | sed -r "s/.*ame=\"([A-Za-z_ ]*)\".*/\1/" | while read i; do
-    patch_xml $FILE "/module/devicePorts/devicePort[@tagName=\"$i\"]/profile[@name=\"\"]"
+else
+  ui_print "   Applying fixes for usb output..."
+  for OFILE in ${APS}; do
+    FILE="$UNITY$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
+    cp_ch -nn $ORIGDIR$OFILE $FILE
+    SPACES=$(sed -n "/^ *usb {/p" $FILE | sed -r "s/^( *).*/\1/")
+    sed -i "/^$SPACES\usb {/,/^$SPACES}/ {/sampling_rates/p; s/\(^ *\)\(sampling_rates .*$\)/\1<!--$MODID\2$MODID-->/g;}" $FILE
+    sed -i "/^$SPACES\usb {/,/^$SPACES}/ s/\(^ *\)sampling_rates .*/\1sampling_rates 48000<!--$MODID-->/g" $FILE
   done
-done
+fi
 
 if $PATCH; then
   ui_print "   Patching existing audio_effects files..."
--- common/uninstall.sh
+++ common/uninstall.sh
@@ -23,4 +23,9 @@ if ! $MAGISK || $SYSOVERRIDE; then
     sed -i "/<!--$MODID-->/d" $FILE
     sed -i -e "s|<!--$MODID\(.*\)|\1|g" -e "s|\(.*\)$MODID-->|\1|g" $FILE
   done
+  for OFILE in ${APS}; do
+    FILE="$UNITY$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
+    sed -i "/<!--$MODID-->/d" $FILE
+    sed -i -e "s|<!--$MODID\(.*\)|\1|g" -e "s|\(.*\)$MODID-->|\1|g" $FILE
+  done
 fi
