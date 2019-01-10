08e8152ffd684c9fdacfdfe806b5500df8bfe0a9
Zackptg5 <zackptg5@comcast.net>
2019-01-10 12:52:26 -0500

    update

--- addon/External-Tools/main.sh
+++
@@ -1,5 +0,0 @@
-# External Tools
-
-chmod -R 0755 $INSTALLER/addon/External-Tools
-cp -R $INSTALLER/addon/External-Tools/tools $INSTALLER/common/unityfiles 2>/dev/null
-[ -d "$INSTALLER/common/unityfiles/tools/other" ] && PATH=$INSTALLER/common/unityfiles/tools/other:$PATH
