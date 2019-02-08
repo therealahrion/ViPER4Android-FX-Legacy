34072127f01c3d509404a8b90e8dac45150c4964
Zackptg5 <zackptg5@comcast.net>
2019-02-07 20:54:47 -0500

    update

--- common/service.sh
+++ common/service.sh
@@ -1,3 +1,7 @@
+while [ ! "$(ps -A | grep -i "<ACTIVITY>")" ] || [ $(getprop sys.boot_completed) -ne 1 ] || [ "$(getprop init.svc.bootanim | tr '[:upper:]' '[:lower:]')" != "stopped" ]; do
+  sleep 1
+done
+sleep 3
 am start -a android.intent.action.MAIN -n <FACTIVITY>
 sleep 1
 killall <ACTIVITY>
