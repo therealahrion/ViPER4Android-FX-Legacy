2f02cb28d0a0d4d72b8e768d1038843f4e02ccdb
Zackptg5 <zackptg5@comcast.net>
2019-03-08 21:57:12 -0500

    Finally found a way to fix bootscript

--- common/service.sh
+++ common/service.sh
@@ -1,5 +1,10 @@
+(until [ $(getprop sys.boot_completed) -eq 1 ]; do
+  sleep 5
+done
 am start -a android.intent.action.MAIN -n <FACTIVITY>
-sleep 1
+until [ "$(pidof <ACTIVITY>)" ]; do
+  sleep 3
+done
 killall <ACTIVITY>
 killall audioserver
-killall mediaserver
+killall mediaserver)&
