9daa140ccc55cef24c94d2fd29be90ffdc83c3b4
Adm0 <perm.malkov@gmail.com>
2019-04-01 20:30:55 +0500

    sepolocy for mtk

--- common/sepolicy.sh
+++ common/sepolicy.sh
@@ -5,5 +5,6 @@ allow { audioserver mediaserver } system_file file execmod
 allow audioserver unlabeled file { read write execute open getattr }
 allow hal_audio_default hal_audio_default process execmem
 allow hal_audio_default hal_audio_default_tmpfs file execute
+allow mtk_hal_audio mtk_hal_audio_tmpfs file execute
 allow hal_audio_default audio_data_file dir search
 allow <SOURCE> app_data_file file execute_no_trans
